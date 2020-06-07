41438cf99ad99eefb9a6623b1054e2b4109803f3
Jia Wei Hong <jiaweihong1@hotmail.com>
2020-06-07 17:57:27 +0800

    added icon images

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,3 @@
 console.log('Content script loaded');
 
-console.log(Date.now());
-
-let time = Math.round(new Date().getTime() / 1000);
-console.log(time);
-console.log(time);
-
-console.log(Math.round(new Date().getTime() / 1000));
 console.log(Math.round(Date.now() / 1000));
