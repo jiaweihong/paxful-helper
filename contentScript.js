88281b2e193c04ca8f6d7702455b09e386fc97d0
Jia Wei Hong <jiaweihong1@hotmail.com>
2020-06-08 21:07:09 +0800

    content script will make an api request using httpRequest object and updated manifest.json to include the api endpoint in permissions

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,14 @@
 console.log('Content script loaded');
 
 console.log(Math.round(Date.now() / 1000));
+
+var getJoke = new XMLHttpRequest();
+
+getJoke.onreadystatechange = function () {
+  if (this.status == 200 && this.readyState == 4) {
+    console.log(2);
+    console.log(this.response);
+  }
+};
+getJoke.open('GET', 'https://api.chucknorris.io/jokes/random', true);
+getJoke.send();
