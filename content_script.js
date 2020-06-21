b8684ce0ad5856ad8970314b9a87dfc843f21ebb
Jia Wei Hong <jiaweihong1@hotmail.com>
2020-06-21 12:29:35 +0800

    get profile links after page has fully loaded

--- content_script.js
+++ content_script.js
@@ -1,6 +1,10 @@
-console.log('Content script loaded');
-
 // Message 1) content_script will send a JSON message to background.js and it will accept a callback function which will execute, if it receives a JSON response from background.js
 chrome.runtime.sendMessage({ type: 'showPageAction' }, function (response) {
   console.log(response.message);
 });
+
+const getProfiles = () => {
+  console.log(document.querySelectorAll('.OfferUser__userInfo'));
+};
+
+window.addEventListener('load', getProfiles);
